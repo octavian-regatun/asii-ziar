3fb5b611e3e1236cba7c3034c08c132f49573844
Valentin Stamate <stamatevalentin125@gmail.com>
2022-10-21 12:45:22 +0300

    Add text over page image

--- pages/editors.js
+++ pages/editors.js
@@ -4,8 +4,12 @@ import EditorsComponent from "../components/Editors";
 const EditorsPage = () => {
     return (
         <div className="w-full flex flex-col items-center gap-4">
-            <div className="w-full max-h-[712px] overflow-hidden">
-                <img className="w-full block overflow-hidden" src="https://i.postimg.cc/pTgFw4b9/370459.jpg" alt=""/>
+            <div className="w-full h-[712px] overflow-hidden relative">
+                <img className="w-full absolute" src="https://i.postimg.cc/pTgFw4b9/370459.jpg" alt=""/>
+                <div className="w-full h-[712px] absolute text-gray-100 flex flex-col justify-center items-center gap-3">
+                    <div className="font-bold text-6xl">Editori</div>
+                    <div className="text-3xl">Lorem ipsum dolor sit amet.</div>
+                </div>
             </div>
 
             <div className="max-layout-width bg-gray-300 p-4">
