955c0b9741d82498f4f50eebb92c25f2ea145127
Octavian Regatun <octavian.regatun@outlook.com>
2022-10-23 02:31:01 +0300

    refactor: NavBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,25 +1,29 @@
-
 export default function NavBar() {
-    return (
-        <nav>
-            <a href="" className="logo"><img src="https://ziar.asii.ro/wp-content/uploads/2017/11/cropped-23899336_1290842091062673_548336396_n.png"></img></a>
-            <ul className="linksNavBar">
-                <li>
-                    <a href="">Acasa</a>
-                </li>
-                <li>
-                    <a href="">Articole</a>
-                </li>
-                <li>
-                    <a href="">Istoric</a>
-                </li>
-                <li>
-                    <a href="">Ziarul Tiparit</a>
-                </li>
-                <li>
-                    <a href="">Echipa Ziarului</a>
-                </li>
-            </ul>
-       </nav>
-    )
+  return (
+    <nav className="navbar">
+      <a href="" className="navbar-logo">
+        <img
+          className="navbar-img"
+          src="https://ziar.asii.ro/wp-content/uploads/2017/11/cropped-23899336_1290842091062673_548336396_n.png"
+        ></img>
+      </a>
+      <ul className="navbar-links">
+        <li className="navbar-link">
+          <a href="">Acasa</a>
+        </li>
+        <li className="navbar-link" S>
+          <a href="">Articole</a>
+        </li>
+        <li className="navbar-link" S>
+          <a href="">Istoric</a>
+        </li>
+        <li className="navbar-link" S>
+          <a href="">Ziarul Tiparit</a>
+        </li>
+        <li className="navbar-link" S>
+          <a href="">Echipa Ziarului</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
-import '../styles/globals.css'
-import '../styles/navBarStyle.css'
+import "../styles/globals.css";
+import "../styles/NavBar.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
