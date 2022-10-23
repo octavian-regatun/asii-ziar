e29d16740ee758ea48e907a91979514f7aaa1a47
AriaWorkshop <ariaworkshopnft@gmail.com>
2022-10-23 16:34:48 +0300

    //page2

--- pages/echipa2.js
+++ pages/echipa2.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Typewriter } from 'react-simple-typewriter'
+
+
 
 const echipa2 = () => {
+
+  const handleType = (count) => {
+    // access word count number
+    console.log(count)}
+  
+
+  const handleDone = () => {
+    console.log(`Done after 5 loops!`)
+  }
+
   return (
-    <div className="text-7xl text-red-500">echipa2</div>
+    <div className="text-7xl text-red-500">
+<Typewriter
+            words={['Ziarul de ASII']}
+            loop={5}
+            cursor
+            cursorStyle='_'
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+            onLoopDone={handleDone}
+            onType={handleType}
+          />
+    </div>
   )
 }
 
