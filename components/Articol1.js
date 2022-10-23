7335ee5bbd98b170d5d7ed2e20ac657109911ef7
AriaWorkshop <ariaworkshopnft@gmail.com>
2022-10-23 18:10:39 +0300

    ULTIMUL PUSH

--- components/Articol1.js
+++ components/Articol1.js
@@ -2,19 +2,18 @@ import React from "react";
 import imagine1 from "./imagine1.jpg";
 import Image from "next/image";
 
-function Articol1() {
+function Articol1(props) {
   return (
     <div className="p-4 m-4 border-2 border-gray-200 drop-shadow-2xl rounded-xl">
       <div className="">
-        <Image src={imagine1} className='rounded-md'/>
+        <Image src={props.imagine} className='rounded-md' />
       </div>
       <div>
         <h4 className="text-xl font-semibold">
-          Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+          {props.title}
         </h4>
         <p className="font-sans text-md">
-          Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum
-          dolor sit amet Lorem ipsum dolor sit amet
+         {props.descriere}
         </p>
         <button className="rounded-md border-2 bg-gray-800 text-white pl-2 pr-2 mt-2 hover:scale-110 transition-transform">
           Read more
--- pages/echipa2.js
+++ pages/echipa2.js
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Typewriter } from 'react-simple-typewriter'
 import Articol1 from '../components/Articol1'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+import { useState } from 'react';
+import imagine1 from "../components/imagine1.jpg";
+import Image from "next/image";
+
 
 
 const echipa2 = () => {
+
+  const [data, setData] = useState([])
+
+
+  const mockdata =[
+    {
+      
+      title: "Despre ASII",
+      descriere: "sdljha ldhas dhsald aslkh dashflkashflkas flkhassdf lkhafls khasflkhasflkh",
+      imagine: "https://www.collegedata.com/hs-fs/collegedata/images/article/what_will_college_life_be_like.jpg"
+  
+  },
+  {
+    title: "Despre ASII",
+    descriere: "sdljha ldhas dhsald aslkh dashflkashflkas flkhassdf lkhafls khasflkhasflkh",
+    imagine: "https://www.bgosoftware.com/blog/wp-content/uploads/2015/10/Featured_photo_BLOG_Humans.jpg"
+  },
+  {
+      
+    title: "Despre ASII",
+    descriere: "sdljha ldhas dhsald aslkh dashflkashflkas flkhassdf lkhafls khasflkhasflkh",
+    imagine: "https://www.collegedata.com/hs-fs/collegedata/images/article/what_will_college_life_be_like.jpg"
+
+},
+{
+  title: "Despre ASII",
+  descriere: "sdljha ldhas dhsald aslkh dashflkashflkas flkhassdf lkhafls khasflkhasflkh",
+  imagine: "https://www.bgosoftware.com/blog/wp-content/uploads/2015/10/Featured_photo_BLOG_Humans.jpg"
+},
+{
+      
+  title: "Despre ASII",
+  descriere: "sdljha ldhas dhsald aslkh dashflkashflkas flkhassdf lkhafls khasflkhasflkh",
+  imagine: "https://www.collegedata.com/hs-fs/collegedata/images/article/what_will_college_life_be_like.jpg"
+
+},
+{
+title: "Despre ASII",
+descriere: "sdljha ldhas dhsald aslkh dashflkashflkas flkhassdf lkhafls khasflkhasflkh",
+imagine: "https://www.bgosoftware.com/blog/wp-content/uploads/2015/10/Featured_photo_BLOG_Humans.jpg"
+},
+{
+  
+title: "Despre ASII",
+descriere: "sdljha ldhas dhsald aslkh dashflkashflkas flkhassdf lkhafls khasflkhasflkh",
+imagine: "https://www.collegedata.com/hs-fs/collegedata/images/article/what_will_college_life_be_like.jpg"
+
+},
+{
+title: "Despre ASII",
+descriere: "sdljha ldhas dhsald aslkh dashflkashflkas flkhassdf lkhafls khasflkhasflkh",
+imagine: "https://www.bgosoftware.com/blog/wp-content/uploads/2015/10/Featured_photo_BLOG_Humans.jpg"
+},
+{
+  title: "Despre ASII",
+  descriere: "sdljha ldhas dhsald aslkh dashflkashflkas flkhassdf lkhafls khasflkhasflkh",
+  imagine: "https://www.bgosoftware.com/blog/wp-content/uploads/2015/10/Featured_photo_BLOG_Humans.jpg"
+  }
+  ];
+
+
+useEffect(() => {
+AOS.init();
+setData(mockdata)
+
+}, [])
 
   const handleType = (count) => {
     // access word count number
@@ -36,19 +108,10 @@ const echipa2 = () => {
           />
         
           </div>
-          <div className='grid grid-cols-3'>
-          <Articol1 />
-          <Articol1 />
-          <Articol1 />
-          <Articol1 />
-          <Articol1 />
-          <Articol1 />
-          <Articol1 />
-          <Articol1 />
-          <Articol1 />
-          <Articol1 />
-          <Articol1 />
-          <Articol1 />
+          <div className='grid grid-cols-3' data-aos="zoom-in">
+        {
+         data ? data.map(({imagine, title, descriere}) => (<Articol1 imagine={imagine1} title={title} descriere={descriere} />)) : <div>nothing</div>
+        }
 
           </div>
     </div>
