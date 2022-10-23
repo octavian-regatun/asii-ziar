50e16e7291d77882632eb3256a0f8a0b44ef437e
puiurobert9441 <puiurobert9441@yahoo.com>
2022-10-23 14:47:52 +0300

    styling editors V2

--- components/Editor.js
+++ components/Editor.js
@@ -3,12 +3,11 @@ import Image from "next/image";
 export default function Editor (props){
     const data=props.data;
     return (
-        <div className=" font-sans drop-shadow-lg text-slate-600 hover:-translate-y-2 duration-500 bg-slate-100 w-40 rounded-lg text-black mx-2 my-3">
-            <Image width='160' height='192' className="shadow-sm rounded-lg mx-auto mb-3" src={data.img} alt={"profile pic off "+ data.name}/>
-            <div className="p-2 pt-1"> 
-                <h4 className="font-semibold text-sm pb-1">{data.name}</h4>
+        <div className=" font-sans text-slate-600 hover:-translate-y-2 duration-500 w-40 rounded-lg text-black mx-2 my-3">
+            <Image width='160' height='192' className="rounded-[15px] mx-auto mb-3" src={data.img} alt={"profile pic off "+ data.name}/>
+            <div className="p-1"> 
+                <h4 className="font-semibold pb-1">{data.name}</h4>
                 <p className="text-xs">{data.role} {data.nrPosts} Posts</p>
-                <p></p>
                 <p className="mt-2 text-xs">{data.text}</p>
              </div>
         </div>
