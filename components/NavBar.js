b0c0102f10a63abecc88054effc7cf294121c727
Pricope Marius-Andrei <enjoycoding10@gmail.com>
2022-10-23 10:19:01 +0300

    make responsive navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,9 +1,15 @@
+import {FaBars} from 'react-icons/fa'
+
+function ActivateList() {
+    console.log("It works")
+}
 
 export default function NavBar() {
     return (
+        <section>
         <nav>
             <a href="" className="logo"><img src="https://ziar.asii.ro/wp-content/uploads/2017/11/cropped-23899336_1290842091062673_548336396_n.png"></img></a>
-            <ul className="linksNavBar">
+            <ul className="linksNavBar" >
                 <li>
                     <a href="">Acasa</a>
                 </li>
@@ -20,6 +26,10 @@ export default function NavBar() {
                     <a href="">Echipa Ziarului</a>
                 </li>
             </ul>
+
+            <button className='barsButton' onClick = {ActivateList}><FaBars/></button>
        </nav>
+
+       </section>
     )
 }
